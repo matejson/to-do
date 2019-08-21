bc2d53eb8ab6ddec6c8501c65c66576de392ebe4
Matejson <mateusz.zaborowskii@gmail.com>
2019-08-21 22:15:30 +0200

    added sticky notes

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ var tasks = [
 ];
 
 // Variables
+
 var newTaskForm = document.querySelector('.new-task-container form');
 var tasksContainer = document.querySelector('.tasks-container ul');
 
 // On DOM load
+
 document.addEventListener('DOMContentLoaded', function() {
 	bindAddTaskEvents();
 	showTasks();
 });
 
 // Add new task
+
 function addNewTask(title) {
 	var taskLi = document.createElement('li');
 	
@@ -39,6 +42,7 @@ function addNewTask(title) {
 }
 
 // Prepare HTML before adding new task
+
 function prepareTaskHTML(title) {
 	return '<div class="input-group">' +
 			'<span class="input-group-btn">' +
@@ -54,6 +58,7 @@ function prepareTaskHTML(title) {
 }
 
 // Handle new task form events
+
 function bindAddTaskEvents() {
 	
 	// On submit
@@ -87,3 +92,86 @@ function deleteTask(deleteBtn) {
 	var liToDelete = deleteBtn.closest('li');
 	deleteBtn.closest('ul').removeChild(liToDelete);
 }
+
+
+//sticky notes
+
+
+
+(function () {
+  'use strict';
+  
+  var draggedEl,
+      onDragStart,
+      onDrag,
+      onDragEnd,
+      grabPointY,
+      grabPointX,
+      createNote,
+      addNoteBtnEl;
+  
+  onDragStart = function (ev) {
+    var boundingClientRect;
+    if (ev.target.className.indexOf('bar') === -1) {
+      return;
+    }
+    
+    draggedEl = this;
+    
+    boundingClientRect = draggedEl.getBoundingClientRect();
+    
+    grabPointY = boundingClientRect.top - ev.clientY;
+    grabPointX = boundingClientRect.left - ev.clientX;
+  };
+  
+  onDrag = function (ev) {
+    if (!draggedEl) {
+      return;
+    }
+    
+    var posX = ev.clientX + grabPointX,
+        posY = ev.clientY + grabPointY;
+    
+    if (posX < 0) {
+      posX = 0;
+    }
+    
+    if (posY < 0) {
+      posY = 0;
+    }
+    
+    draggedEl.style.transform = "translateX(" + posX + "px) translateY(" + posY + "px)";
+  };
+  
+  onDragEnd = function () { 
+    draggedEl = null;
+    grabPointX = null;
+    grabPointY = null;
+  };
+  
+  createNote = function () {
+    var stickerEl = document.createElement('div'),
+        barEl = document.createElement('div'),
+        textareaEl = document.createElement('textarea');
+    
+    var transformCSSValue = "translateX(" + Math.random() * 400 + "px) translateY(" + Math.random() * 400 + "px)";
+    
+    stickerEl.style.transform = transformCSSValue; 
+    
+    barEl.classList.add('bar');
+    stickerEl.classList.add('sticker');
+    
+    stickerEl.appendChild(barEl);
+    stickerEl.appendChild(textareaEl); 
+    
+    stickerEl.addEventListener('mousedown', onDragStart, false);
+    
+    document.body.appendChild(stickerEl);
+  };
+ 
+  
+addNoteBtnEl = document.querySelector('.addNoteBtn');
+  addNoteBtnEl.addEventListener('click', createNote, false);
+  document.addEventListener('mousemove', onDrag, false);
+  document.addEventListener('mouseup', onDragEnd, false);
+})();
